83f82c2759fa7d1fdb50cd77e097c0037ea8d447
Ahmed Saliu <saliu.lito@gmail.com>
2020-08-29 04:48:29 +0000

    pure recursion practice

---
+++ index.js
@@ -0,0 +1,15 @@
+function collectOldValues(arr){
+  let result = [];
+
+  if(arr.length === 0) return result;
+
+  if(arr[0] % 2 !== 0){
+    result.push(arr[0]);
+  }
+
+  result = result.concat(collectOldValues(arr.slice(1)));
+  return result
+}
+
+
+collectOldValues([1,2,3,4,5,6,7,9,11,10])
